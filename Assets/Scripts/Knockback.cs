f678fb6a7d67e0461377b40ed306de4a80a9fae9
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-02-12 20:01:22 +0300

    hiig

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Knockback : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private Animator _animator;
+
+    [Header("Settings")]
+    [SerializeField] private float knockbackForce = 10f;
+    [SerializeField] private float knockbackDuration = 0.5f;
+
+    private Rigidbody2D rb;
+    private float knockbackTimer;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (knockbackTimer > 0)
+        {
+            knockbackTimer -= Time.deltaTime;
+            if (knockbackTimer <= 0)
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            TakeDamage(new Vector2(1,0));
+            _animator.SetTrigger("Hit");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(new Vector2(-1,0));
+            _animator.SetTrigger("Hit");
+        }
+    }
+
+    public void TakeDamage(Vector2 knockbackDirection)
+    {
+        rb.velocity = knockbackDirection * knockbackForce;
+
+        knockbackTimer = knockbackDuration;
     }
 }
