6e90459326439e954f1097db5fc47695c81b2189
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-02-16 00:05:30 +0300

    small changes
    -death delay fixed
    -changed the collider detection type of heart utility oncollision to ontrigger (this way feeling is better)

--- Assets/Scripts/Heal.cs
+++ Assets/Scripts/Heal.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Heal : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if (collision.transform.tag == "Player")
         {
             collision.gameObject.GetComponent<Health>().TakeHeal();
 
